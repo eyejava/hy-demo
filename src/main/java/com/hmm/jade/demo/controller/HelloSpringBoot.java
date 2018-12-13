13cf7c0ef2ad88f3a8fc59f24c3422bf3d95aeec
eyejava <eyejava@163.com>
2018-12-13 11:37:41 +0800

    test

---
+++ src/main/java/com/hmm/jade/demo/controller/HelloSpringBoot.java
@@ -0,0 +1,27 @@
+package com.hmm.jade.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloSpringBoot {
+
+    @RequestMapping(value = "/hello")
+    public String say() {
+        return "hello jade";
+    }
+
+    @RequestMapping(value = "/say/{id}")
+    public String sayId(@PathVariable("id") Integer id) {
+        return "id = " + id;
+    }
+
+    @RequestMapping(value = "/param")
+    public String sayParam(@RequestParam(value = "id", required = false, defaultValue = "0") Integer myId) {
+        return "myId = " + myId;
+    }
+}
